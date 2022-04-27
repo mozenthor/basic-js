4232fd0a031e80b8f4ed54cc9b5b3ecc5cb85911
Aleksandr Karapetov <karapet.yo@gmail.com>
2022-04-28 00:19:03 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string"){
+    return false
+  }
+  let x = Number(sampleActivity)
+  if (isNaN(x)){
+    return false
+  }
+  if(x > 15 || x <= 0){
+      return false
+  }   
+  let res = Math.log(MODERN_ACTIVITY/x)/(0.693/HALF_LIFE_PERIOD)
+  return Math.ceil(res)                
 }
 
 module.exports = {
